8648815109a6d45eacf16e500036d07873b333ee
Laurenz Seidel <LaurenzSeidel@gmail.com>
2023-05-30 14:56:32 +0300

    use dynamic youtube video url instead of hard coded video id

--- components/SerenePlayer.tsx
+++ components/SerenePlayer.tsx
@@ -1,50 +1,73 @@
 "use client";
 import YouTubeAudioPlayer from "../components/YouTubeAudioPlayer";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import LoadingDots from "@/components/LoadingDots";
-const SerenePlayer = () =>  {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const videoId = "3r_Z5AYJJd4";
+import { getSoundscapeLink } from "../lib/soundscapes";
+import { getYouTubeIdFromURL } from "../lib/youtube_utils";
 
-    const onReady = () => {
-        setIsLoading(false);
-    }
+const SerenePlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [youTubeId, setYouTubeId] = useState<string>();
 
-    const onWaiting = () => {
-        setIsLoading(true);
-    }
+  const onReady = () => {
+    setIsLoading(false);
+  };
 
-    const onResumedPlaying = () => {
-        setIsLoading(false);
-    }
+  const onWaiting = () => {
+    setIsLoading(true);
+  };
 
-    const handleClick = (event: MouseEvent) => {
-        // Handle the click event only if it has not been processed by other components
-        setIsPlaying(!isPlaying);
-        console.log('Clicked anywhere on the screen');
+  const onResumedPlaying = () => {
+    setIsLoading(false);
+  };
+
+  const handleClick = (event: MouseEvent) => {
+    setIsPlaying(!isPlaying);
+  };
+
+  useEffect(() => {
+    // Attach the click event listener to the document during the capture phase
+    // to handle the click event only if it has not been processed by other components
+    document.addEventListener("click", handleClick, { capture: true });
+
+    console.log(getSoundscapeLink());
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener("click", handleClick, { capture: true });
+    };
+  });
+
+  useEffect(() => {
+    const setYouTubeURL = async () => {
+      try {
+        const youtube_url = await getSoundscapeLink();
+        const youtube_id = getYouTubeIdFromURL(youtube_url);
+        setYouTubeId(youtube_id);
+      } catch (error) {
+        console.error("Failed to fetch YouTube video data:", error);
+      }
     };
 
-    useEffect(() => {
-        // Attach the click event listener to the document during the capture phase
-        document.addEventListener('click', handleClick, { capture: true });
+    setYouTubeURL();
+  });
 
-        // Clean up the event listener when the component unmounts
-        return () => {
-            document.removeEventListener('click', handleClick, { capture: true });
-        };
-    });
-
-    return (
-        <div>
-            {!isPlaying && ("Muted")}
-            {isLoading && (
-                <LoadingDots/>
-            )}
-            <YouTubeAudioPlayer videoId={videoId}  onReady={onReady} onWaiting={onWaiting} onResumed={onResumedPlaying} isPlaying={isPlaying}/>
-        </div>
-    );
-
-}
+  return (
+    <div>
+      {!isPlaying && "Muted"}
+      {isLoading && <LoadingDots />}
+      {youTubeId && (
+        <YouTubeAudioPlayer
+          videoId={youTubeId}
+          onReady={onReady}
+          onWaiting={onWaiting}
+          onResumed={onResumedPlaying}
+          isPlaying={isPlaying}
+        />
+      )}
+    </div>
+  );
+};
 
 export default SerenePlayer;
